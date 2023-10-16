e2d1a2735c25f0b64253eaf258ed543f60b1353b
kennycashmoney <ken.larose@proton.me>
2023-10-16 09:36:35 -0500

    app.js serves up the mounted react prod-build and morgan logging for http requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,28 +1,22 @@
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 
-// const cors = require("cors");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 
-// const planetsRouter = require("./routes/planets/planets.router");
-// const launchesRouter = require("./routes/launches/launches.router");
 const habitsRouter = require('./routes/habits.router')
 
 const app = express();
 
-// app.use(cors({ origin: "http://localhost:3000" }));
-// app.use(morgan("combined"));
+app.use(morgan("combined"));
 
 app.use(express.json()); // parse json from body of incoming requests
-// app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(habitsRouter);
 
-// app.use("/planets", planetsRouter);
-// app.use("/launches", launchesRouter);
-
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-// });
+app.get("/*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 
 module.exports = app;
